94107ef0ea0b85e563f8833660175c2bd9375d5d
Tobias Bohn <info@tobiasbohn.com>
2022-03-30 21:40:48 +0200

    added name input field

--- app/javascript/channels/snake_channel.js
+++ app/javascript/channels/snake_channel.js
@@ -15,35 +15,60 @@ const channel = consumer.subscriptions.create("SnakeChannel", {
 
   received(data) {
     // Called when there's incoming data on the websocket for this channel
-    console.log(data);
+    // console.log(data);
     drawSnakeGame(data);
   }
 });
 
+
+
+const inputHandler = function(e) {
+  // console.log(e.target.value);
+  channel.send({ type: "name", body: e.target.value });
+}
+
+var inputFieldHasFocus = false;
+var inputElement = document.getElementById("snakeNameInput");
+
+inputElement.addEventListener('focus', (event) => {
+  inputFieldHasFocus = true;
+})
+inputElement.addEventListener('focusout', (event) => {
+  inputFieldHasFocus = false;
+})
+
+inputElement.addEventListener('input', inputHandler);
+inputElement.addEventListener('propertychange', inputHandler);
+
+
+
+
 document.addEventListener('keydown', (event) => {
-  var anyInput = false;
+  if (!inputFieldHasFocus) {
+    var anyInput = false;
 
-  if (event.code == "ArrowDown" || event.code == "KeyS") {
-    channel.send({ type: "movement", body: "down" });
-    anyInput = true;
-  }
+    if (event.code == "ArrowDown" || event.code == "KeyS") {
+      channel.send({ type: "movement", body: "down" });
+      anyInput = true;
+    }
 
-  if (event.code == "ArrowUp" || event.code == "KeyW") {
-    channel.send({ type: "movement", body: "up" });
-    anyInput = true;
-  }
+    if (event.code == "ArrowUp" || event.code == "KeyW") {
+      channel.send({ type: "movement", body: "up" });
+      anyInput = true;
+    }
 
-  if (event.code == "ArrowLeft" || event.code == "KeyA") {
-    channel.send({ type: "movement", body: "left" });
-    anyInput = true;
-  }
+    if (event.code == "ArrowLeft" || event.code == "KeyA") {
+      channel.send({ type: "movement", body: "left" });
+      anyInput = true;
+    }
 
-  if (event.code == "ArrowRight" || event.code == "KeyD") {
-    channel.send({ type: "movement", body: "right" });
-    anyInput = true;
-  }
+    if (event.code == "ArrowRight" || event.code == "KeyD") {
+      channel.send({ type: "movement", body: "right" });
+      anyInput = true;
+    }
 
-  if (anyInput) {
-    event.preventDefault();
+    if (anyInput) {
+      event.preventDefault();
+    }
   }
 }, false);
